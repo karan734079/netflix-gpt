680399cc1d7182870b3a6fb320090852820b283c
karan kumar <karanjangral60@gmail.com>
2024-07-26 15:33:12 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-sans text-3xl flex justify-center font-bold text-orange-600">
+      <p>hello react</p>
     </div>
   );
 }
